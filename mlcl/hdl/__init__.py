7b5bfc315e03707a2f8d40c5347f97263b0e8913
cappuch <ilovevisualstudiocode@gmail.com>
2025-01-05 23:51:43 +0000

    fixed one dookieballs thing and a web demo

--- mlcl/hdl/__init__.py
+++ mlcl/hdl/__init__.py
@@ -1,5 +1,8 @@
-from .generator import HDLGenerator
-from .layers import LinearHDL, Conv2DHDL
 from .pipeline import ModelToHDLPipeline
+from .layers import LinearHDL, Conv2DHDL
 
-__all__ = ['HDLGenerator', 'LinearHDL', 'Conv2DHDL', 'ModelToHDLPipeline'] 
+__all__ = [
+    'ModelToHDLPipeline',
+    'LinearHDL',
+    'Conv2DHDL',
+]
